19ae59c4da73d86d1a82fb0607033cc8456ba5b6
[Arin Ramer] <[arinramer@gmail.com]>
2020-02-12 18:36:14 -0800

    Done

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // code away!
+const express = require("express");
+
+const postRouter = require("./posts/postRouter");
+
+const userRouter = require("./users/userRouter");
+
+const server = express();
+
+server.use(logger);
+
+server.use(express.json());
+
+server.use("/api/posts", postRouter);
+
+server.use("/api/users", userRouter);
+
+function logger(req, res, next) {
+	console.log(
+		`[${new Date().toISOString()}] ${req.method} to ${req.url}}`
+	);
+	next();
+}
+
+server.listen(5000, () => {
+	console.log("\n* Server Running on http://localhost:5000 *\n");
+});
--- posts/postRouter.js
+++ posts/postRouter.js
@@ -2,26 +2,73 @@ const express = require('express');
 
 const router = express.Router();
 
+const posts = require("./postDb");
+
 router.get('/', (req, res) => {
-  // do your magic!
+  posts.get().then(posts => {
+    res.status(200).json(posts);
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json({ message: "Unable to fulfill request" });
+  })
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+router.get('/:id', validatePostId, (req, res) => {
+  res.status(200).json(req.post);
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
+router.delete('/:id', validatePostId, (req, res) => {
+  const id = req.params.id;
+  posts.remove(id)
+  .then(deleted => {
+    res.status(200).json(deleted);
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json({ error: "Unable to fulfill request"})
+  })
 });
 
-router.put('/:id', (req, res) => {
-  // do your magic!
+router.put('/:id', validatePostId, validatePost, (req, res) => {
+  const id = req.params.id;
+  const body = req.body;
+  posts.update(id, body)
+  .then(updated => {
+    res.status(200).json(updated);
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json({ error: "Unable to fulfill request"})
+  })
 });
 
 // custom middleware
 
 function validatePostId(req, res, next) {
-  // do your magic!
+  const id = req.params.id;
+  posts.getById(id)
+  .then(post => {
+    if(post !== undefined) {
+      req.post = post;
+      next();
+    } else {
+      res.status(400).json({ message: "Invalid post id" });
+    }
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json({ message: "Unable to fulfill request" });
+  })
+}
+
+function validatePost(req, res, next) {
+  const text = req.body.text;
+  const body = req.body;
+  if(JSON.stringify(body) === '{}') {
+    res.status(400).json({ message: "missing post data" });
+  } else if(JSON.stringify(body) !== '{}' && !text) {
+    res.status(400).json({ message: "missing required text field" });
+  } else {
+    req.body = body;
+    next();
+  }
 }
 
 module.exports = router;
